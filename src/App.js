f6af6180bc6e51f898dbd05b9d8eee8e07e8c861
Edrian <mr.edrian@gmail.com>
2022-12-28 15:43:59 +0800

    version 1

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Toaster } from 'react-hot-toast';
+import AppContent from './components/AppContent';
+import AppHeader from './components/AppHeader';
+import PageTitle from './components/PageTitle';
+import styles from './styles/modules/app.module.scss';
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="test">
+        <PageTitle>Calendar App</PageTitle>
+        </div>
+        <div className={styles.app__wrapper}>
+          <AppHeader />
+          <AppContent />
+        </div>
+      </div>
+      <Toaster
+        position="bottom-right"
+        toastOptions={{
+          style: {
+            fontSize: '1.4rem',
+          },
+        }}
+      />
+    </>
+  );
+}
+
+export default App;
